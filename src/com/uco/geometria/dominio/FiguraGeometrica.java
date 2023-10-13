d8a4d4b6d33533c6abab433698ae17a63c72bd0e
Sebastian <sebasbote18@gmail.com>
2023-10-12 19:00:00 -0500

    Merge branch 'master' of C:\Users\sebas\OneDrive\Documentos\GitHub\poo202302 with conflicts.

--- src/com/uco/geometria/dominio/FiguraGeometrica.java
+++ src/com/uco/geometria/dominio/FiguraGeometrica.java
@@ -1,6 +1,10 @@
 package com.uco.geometria.dominio;
 
 public abstract class FiguraGeometrica {
-    public abstract double calcularArea();
-    public abstract double calcularPerimetro();
+
+    public calcularArea();
+
+    public double calcularPerimetro() {
+        return 0;
+    }
 }
--- src/com/uco/geometria/dominio/Triangulo.java
+++ src/com/uco/geometria/dominio/Triangulo.java
@@ -1,4 +1,29 @@
 package com.uco.geometria.dominio;
 
-public abstract class Triangulo extends FiguraGeometrica {
+public class Triangulo extends FiguraGeometrica {
+    private int base;
+
+    private int altura;
+
+    private int lado1;
+
+    private int lado2;
+
+    public Triangulo(int base, int altura, int lado1, int lado2) {
+        this.base = base;
+        this.altura = altura;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    @Override
+    public double calcularArea(){
+        return (this.base * (double)this.altura) / 2;
+    }
+
+    @Override
+    public double calcularPerimetro(){
+        return (this.lado1 + this.lado2) + this.base;
+    }
+
 }
